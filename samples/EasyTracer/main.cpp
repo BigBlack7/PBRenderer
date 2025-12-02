ec272940e3a37e485d0d65866fcb3ef7c48402e9
bigblack <bigblack@qq.com>
2025-12-02 18:43:01 +0800

    测试 <OpenEXR>

--- samples/EasyTracer/main.cpp
+++ samples/EasyTracer/main.cpp
@@ -4,10 +4,18 @@
 #include <spdlog/spdlog.h>
 #include <logger.hpp>
 #include <sfml/graphics.hpp>
+#include <ImfHeader.h>
+#include <ImfChannelList.h>
+#include <ImfOutputFile.h>
+#include <ImfInputFile.h>
+#include <ImfFrameBuffer.h>
 int main()
 {
     core::Logger::Init();
-    sf::RenderWindow window;
+    sf::RenderWindow window(
+        sf::VideoMode({800, 600}),
+        "My first Window",
+        sf::Style::Resize);
 
     PBRT_INFO("Hello, PBRT!");
 
